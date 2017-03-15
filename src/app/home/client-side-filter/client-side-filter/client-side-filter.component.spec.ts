21687710117b8fc43b3e543402e6985353770d23
KwintenP <kwinten.pisman@gmail.com>
2017-03-15 20:08:13 +0100

    Fixed the testing with the latest angular cli version. Finally

--- src/app/home/client-side-filter/client-side-filter/client-side-filter.component.spec.ts
+++ src/app/home/client-side-filter/client-side-filter/client-side-filter.component.spec.ts
@@ -1,26 +1,13 @@
-import {Observable} from "rxjs/Observable";
 import "rxjs/add/observable/from";
 import "rxjs/add/operator/filter";
 import "rxjs/add/operator/merge";
 import "rxjs/add/operator/take";
-import "./helpers/test-helper.ts";
-import {cold, expectObservable, expectSubscriptions} from "./helpers/marble-testing";
+import "../../../common/helpers/test-helper";
+import {cold, expectObservable, expectSubscriptions} from "../../../common/helpers/marble-testing";
 
-// A few basic examples to get started
-describe('The filter operator', () => {
-  it('should correctly filter non-even numbers', () => {
-    console.log("called");
-    const source = Observable.from<number>([1, 2, 3, 4, 5]);
-    const example = source.filter(num => num % 2 === 0);
-    const values = {a: 2, b: 4};
-    const expected = '(ab|)';
-
-    expectObservable(example).toBe(expected, values);
-  });
-});
 // Example with expectSubscription
-describe('The merge operator', () => {
-  it('should merge two observables', () => {
+describe('component: ClientSideFilterComponent', () => {
+  it('on createFilterCharacters', () => {
     const values = {a: 1, b: 2, c: 3, d: 4};
     const a = cold(' a-----b-----c----|', values)
     const asub = ( '^-----------------!')
