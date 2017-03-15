cdde08229e629719e36cb555e9e1b6654a5522cc
KwintenP <kwinten.pisman@gmail.com>
2017-03-15 19:19:37 +0100

    Replaced the file to the correct folder and name.

--- src/app/home/client-side-filter/client-side-filter/client-side-filter.component.spec.ts
+++ src/app/home/client-side-filter/client-side-filter/client-side-filter.component.spec.ts
@@ -1,70 +1,35 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/from";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/merge";
+import "rxjs/add/operator/take";
+import "./helpers/test-helper.ts";
+import {cold, expectObservable, expectSubscriptions} from "./helpers/marble-testing";
 
-import { ClientSideFilterComponent } from './client-side-filter.component';
-import {GenderFilterComponent} from "../gender-filter/gender-filter.component";
-import {CharacterListComponent} from "../character-list/character-list.component";
-import {StarWarsService} from "../../../common-logic/services/star-wars.service";
-import {CommonModule} from "@angular/common";
+// A few basic examples to get started
+describe('The filter operator', () => {
+  it('should correctly filter non-even numbers', () => {
+    console.log("called");
+    const source = Observable.from<number>([1, 2, 3, 4, 5]);
+    const example = source.filter(num => num % 2 === 0);
+    const values = {a: 2, b: 4};
+    const expected = '(ab|)';
 
+    expectObservable(example).toBe(expected, values);
+  });
+});
+// Example with expectSubscription
+describe('The merge operator', () => {
+  it('should merge two observables', () => {
+    const values = {a: 1, b: 2, c: 3, d: 4};
+    const a = cold(' a-----b-----c----|', values)
+    const asub = ( '^-----------------!')
+    const b = cold('---------d----------|', values)
+    const bsub = '^-------------------!'
+    const expected = '-a-----b-d---c------|'
 
-import { hot, cold, expectObservable, expectSubscriptions } from '../../../common//helpers/marble-testing';
-
-import "../../../common/helpers/test-helper.ts";
-import createSpyObj = jasmine.createSpyObj;
-import Spy = jasmine.Spy;
-fdescribe('ClientSideFilterComponent', () => {
-  let component: ClientSideFilterComponent;
-  let fixture: ComponentFixture<ClientSideFilterComponent>;
-  let starWarsService: StarWarsService;
-
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ ClientSideFilterComponent, GenderFilterComponent, CharacterListComponent ],
-  //     providers: [StarWarsService],
-  //     imports: [CommonModule]
-  //   })
-  //   .compileComponents();
-  // }));
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(ClientSideFilterComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-  //
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
-
-  beforeEach(() => {
-    starWarsService = createSpyObj("starWarsService", ["getCharacters"]);
-
-    component = new ClientSideFilterComponent(starWarsService);
-  })
-
-  it("should calculate the thing correctly", () => {
-    const values = {a: "ALL", b: "MALE", c: "FEMALE", d: "All"};
-    const filter$ = cold("a---b----c--d---|", values);
-
-    const obiWan = {
-      name: "Obi Wan",
-      gender: "FEMALE"
-    };
-    const yoda = {
-      name: "Yoda",
-      gender: "Male"
-    };
-    const C3PO = {
-      name: "c3po",
-      gender: "N/A"
-    };
-    const characters$ = cold("-----a|", {a :[obiWan, yoda, C3PO]});
-
-    const result$ = component.createFilterCharacters(filter$, characters$);
-
-    expectObservable(result$).toBe("-----a---b--c---|", {a:[yoda], b:[obiWan], c:[obiWan, yoda, C3PO]});
+    expectObservable(a.merge(b).take(5)).toBe(expected, values);
+    expectSubscriptions(a.subscriptions).toBe(asub);
+    expectSubscriptions(b.subscriptions).toBe(bsub);
   });
 });
