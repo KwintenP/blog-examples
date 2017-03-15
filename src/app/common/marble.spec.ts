dc64176ca1d9d4cd3088c80e97c2f15f6a09ccdc
KwintenP <kwinten.pisman@gmail.com>
2017-03-15 19:12:21 +0100

    Fixed tslint in the marble testing.

--- src/app/common/marble.spec.ts
+++ src/app/common/marble.spec.ts
@@ -1,18 +1,16 @@
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/from';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/merge';
-import 'rxjs/add/operator/take';
-
-import { hot, cold, expectObservable, expectSubscriptions } from './helpers/marble-testing';
-
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/from";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/merge";
+import "rxjs/add/operator/take";
 import "./helpers/test-helper.ts";
+import {cold, expectObservable, expectSubscriptions} from "./helpers/marble-testing";
 
-//A few basic examples to get started
+// A few basic examples to get started
 describe('The filter operator', () => {
   it('should correctly filter non-even numbers', () => {
     console.log("called");
-    const source = Observable.from<number>([1,2,3,4,5]);
+    const source = Observable.from<number>([1, 2, 3, 4, 5]);
     const example = source.filter(num => num % 2 === 0);
     const values = {a: 2, b: 4};
     const expected = '(ab|)';
@@ -20,15 +18,15 @@ describe('The filter operator', () => {
     expectObservable(example).toBe(expected, values);
   });
 });
-//Example with expectSubscription
+// Example with expectSubscription
 describe('The merge operator', () => {
   it('should merge two observables', () => {
     const values = {a: 1, b: 2, c: 3, d: 4};
     const a = cold(' a-----b-----c----|', values)
     const asub = ( '^-----------------!')
     const b = cold('---------d----------|', values)
-    const bsub =   '^-------------------!'
-    const expected='-a-----b-d---c------|'
+    const bsub = '^-------------------!'
+    const expected = '-a-----b-d---c------|'
 
     expectObservable(a.merge(b).take(5)).toBe(expected, values);
     expectSubscriptions(a.subscriptions).toBe(asub);
