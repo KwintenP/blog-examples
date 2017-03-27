bd399e3e502e09b9b3ea69b4aa430b9a287b4f02
KwintenP <kwinten.pisman@gmail.com>
2017-03-27 20:09:44 +0200

    Trying to add devtools.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {TopNavComponent} from "./top-nav/top-nav.component";
 import {RouterModule, Routes} from "@angular/router";
-import {HomeComponent} from "./home/home/home.component";
 import {HomeModule} from "./home/home.module";
-import { SideNavComponent } from './home/side-nav/side-nav.component';
+// TODO: remove
+import {Action, StoreModule} from "@ngrx/store";
+import {StoreLogMonitorModule} from "@ngrx/store-log-monitor";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/home'},
@@ -19,9 +20,29 @@ const routes: Routes = [
     path: 'about',
     loadChildren: 'app/about/about.module#AboutModule'
   },
-  { path: '**', redirectTo: '/home'}
+  {path: '**', redirectTo: '/home'}
 ];
 
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const RESET = 'RESET';
+
+export function counterReducer(state = 0, action: Action): number {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    case RESET:
+      return 0;
+
+    default:
+      return state;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -33,9 +54,13 @@ const routes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(routes, {enableTracing: false}),
-    HomeModule
+    HomeModule,
+    StoreModule.provideStore({counter: counterReducer}),
+    StoreDevtoolsModule.instrumentStore({}),
+    StoreLogMonitorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
