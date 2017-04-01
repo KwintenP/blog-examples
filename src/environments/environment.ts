57a8d571e2e24ba4871fab23be32dc6d3506c616
KwintenP <kwinten.pisman@gmail.com>
2017-04-01 21:28:03 +0200

    Fixed the aot compilation.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,20 +4,18 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {StoreLogMonitorModule, useLogMonitor} from "@ngrx/store-log-monitor";
+import {useDockMonitor} from "@ngrx/store-log-monitor/src/dock-monitor";
 export const environment = {
   production: false
 };
 
-export const logMonitor = () =>
-  useLogMonitor({
-    visible: false,
-    position: "right"
-  });
-
+export function instrumentOptions() {
+  return {
+    monitor: useLogMonitor({ visible: true, position: 'right' })
+  };
+}
 
 export const extraModules = [
-  StoreDevtoolsModule.instrumentStore({
-    monitor: logMonitor()
-  }),
+  StoreDevtoolsModule.instrumentStore(instrumentOptions),
   StoreLogMonitorModule
 ];
