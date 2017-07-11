f10bbcf06f7a7c41be00987dc5185153eddf6eac
KwintenP <kwinten.pisman@gmail.com>
2017-07-11 21:03:50 +0200

    Added rxdevtools as a test.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { setupRxDevtools } from 'rx-devtools/rx-devtools';
 
 if (environment.production) {
   enableProdMode();
+} else {
+  setupRxDevtools();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
