2ac4f31157454de4d69cba1dbd8bb09aff8f1016
KwintenP <kwinten.pisman@gmail.com>
2017-07-20 21:00:58 +0200

    Updated the test obs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'uuid';
 import 'rx-devtools/add/operator/debug';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,10 @@ export class AppComponent implements OnInit {
   title = 'app works!';
 
   ngOnInit() {
-    Observable.interval(1000).debug('interval').subscribe();
+    Observable.interval(1000)
+      .debug('interval')
+      .take(10)
+      .map((val: number) => val * 2)
+      .subscribe();
   }
 }
