c0d177ae94b199b662217195adafc65a12138fba
KwintenP <kwinten.pisman@gmail.com>
2017-07-22 10:03:04 +0200

    Updated the test observable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,6 +36,7 @@ export class AppComponent implements OnInit {
       .debug('combined')
       .map(([interval, other]) => {
       return interval * other;
-    });
+    })
+      .subscribe(() => {});
   }
 }
