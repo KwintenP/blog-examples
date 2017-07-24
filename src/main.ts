3286799d061ba30ebf7310ebf8b1024d2ecd17e4
KwintenP <kwinten.pisman@gmail.com>
2017-07-24 22:06:23 +0200

    Trying to add subscription counters.

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { setupRxDevtools } from 'rx-devtools/rx-devtools';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
 
 if (environment.production) {
   enableProdMode();
 } else {
+  const monkeyPatchSubscribe = function() {
+    const originalSubscribe = Observable.prototype.subscribe;
+    Observable.prototype.subscribe = function (...args) {
+      console.log('subscribed!');
+      const subscription =  originalSubscribe.apply(this, args);
+      (subscription as any).id = 'testes';
+      return subscription;
+    };
+
+    const originalUnsubscribe = Subscription.prototype.unsubscribe;
+    Subscription.prototype.unsubscribe = function(...args) {
+      console.log('unsubscribed', this);
+      return originalUnsubscribe.apply(this, args);
+    };
+  }
+  monkeyPatchSubscribe();
   setupRxDevtools();
 }
 
